a1973960250d946e9bfd7808760f5df82a283323
mbali17 <alimirbasheer@gmail.com>
2020-02-05 11:36:19 -0800

    adding the strings and variables program.

---
+++ AMJSCOURSE/variableandflowcontrol/stringsandvariables.js
@@ -0,0 +1,7 @@
+//to declare a variable we use the reserved keyword 'let' .
+let firstName = 'tom';
+let lastName = 'hardy';
+//to concatenate we use the '+' operator
+let fullName = firstName + ' ' + lastName;
+//to print we use the console.log statement.
+console.log(fullName);

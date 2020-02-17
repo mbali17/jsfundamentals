0df8b7e195ca2f7ef8e6a5aedd9fb4eaaf337b68
mbali17 <alimirbasheer@gmail.com>
2020-02-17 11:54:26 -0800

    boolean in js

---
+++ AMJSCOURSE/basics/boolean.js
@@ -0,0 +1,8 @@
+/*
+boolean can only have two potential values either true or false. these values are a result of comparison using comparison operators ('===','!==','<','>') avaialable.
+*/
+let age = 65;
+let isChild = age <= 7;
+let isSenior = age >= 65;
+console.log(isChild);
+console.log(isSenior);

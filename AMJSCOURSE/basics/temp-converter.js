825bfc55d730b62767f70fc9bf0cee059547cf88
mbali17 <alimirbasheer@gmail.com>
2020-02-17 09:11:09 -0800

    numbers in js

---
+++ AMJSCOURSE/basics/temp-converter.js
@@ -0,0 +1,5 @@
+let fahrenheit = 32;
+let celcius = (fahrenheit - 32) * (5 / 9);
+let kelvin = (fahrenheit + 459.67) * (5 / 9);
+console.log(fahrenheit + 'f = ' + celcius + 'c');
+console.log(fahrenheit + 'f = ' + kelvin + 'k');
